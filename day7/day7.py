62b86583054292367285756ae5f98a64ce275d86
Gwen Bradforth <gbradfor@usc.edu>
2024-12-15 00:40:56 -0600

    day7 part2
    Co-authored-by: Sam Johnson-Lacoss <samgjl@users.noreply.github.com>

--- day7/day7.py
+++ day7/day7.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 
 # RETURNS:
 # [
@@ -19,7 +20,7 @@ def part1(path: str) -> int:
     equations = read_input(path)
     
     total = 0
-    for e in equations:
+    for e in tqdm(equations):
         tar,comp = e
         if check_valid(tar,comp):
             total += tar
@@ -27,15 +28,34 @@ def part1(path: str) -> int:
     return total
 
 # Recursive function to check if there are any combos that would work
-def check_valid(tar: int, comp: list[int], curr: int = 0,):
+def check_valid(tar: int, comp: list[int], curr: int = 0):
     if len(comp) == 0:
         return tar == curr
     else:
         plus = check_valid(tar,comp[1:],curr+comp[0]) if curr+comp[0]<=tar else False
         times = check_valid(tar,comp[1:], curr*comp[0]) if curr*comp[0]<=tar else False
         return plus or times
+    
+def check_valid2(tar: int, comp: list[int], curr: int = 0):
+    if len(comp) == 0:
+        return tar == curr
+    else:
+        plus = check_valid2(tar,comp[1:],curr+comp[0]) if curr+comp[0]<=tar else False
+        times = check_valid2(tar,comp[1:], curr*comp[0]) if curr*comp[0]<=tar else False
+        concat = int(str(curr) + str(comp[0]))
+        concat = check_valid2(tar,comp[1:],concat) if concat<=tar else False
+        return plus or times or concat
 
 def part2(path: str) -> int:   
+    equations = read_input(path)
+
+    total = 0
+    for e in tqdm(equations):
+        tar,comp = e
+        if check_valid2(tar,comp):
+            total += tar
+        
+    return total
     pass
 
 if __name__ == "__main__":
