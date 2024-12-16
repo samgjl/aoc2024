988dc98d5c9027729f68dbf23a2d88698f51bc4a
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2024-12-16 00:08:26 -0600

    Day 9 part 1

--- day9/day9.py
+++ day9/day9.py
@@ -2,28 +2,48 @@ def read_input(path: str):
     with open(path) as f:
         return f.read()
 
-def filesystem(og: str):
-    fs = ""
+def filesystem(og: str) -> list[int|str]:
+    fs = []
     id = 0
     block = True
     for char in og:
         if block:
-            fs += (f"{id}"*int(char))
+            fs += ([id]*int(char))
             id += 1
         else:
-            fs += ("."*int(char))
+            fs += (["."]*int(char))
         block = not block
     return fs
-        
 
+def optimize(fs: list[str]):
+    fs = fs[::]
+    left = 0
+    right = len(fs)-1
+
+    while True:
+        while fs[left] != ".":
+            left += 1
+        while fs[right] == ".":
+            right -= 1
+        if left >= right:
+            return fs
+        fs[left] = fs[right]
+        fs[right] = "."
+
+def checksum(fs: list[str]) -> int:
+    total = 0
+    for i in range(len(fs)):
+        if fs[i] != ".":
+            total += int(fs[i]) * i
+    return total
 
 
 def part1(path: str):
     original = read_input(path)
     fs = filesystem(original)
+    opt = optimize(fs)
+    return checksum(opt)
     
-    print(fs)
-
 def part2(path: str):
     pass
 
