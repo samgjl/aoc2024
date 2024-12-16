8e8b2662b7c16a3390086f8056a9ebef0d8264f9
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2024-12-15 23:31:31 -0600

    Day 8 -- part 2. It ain't pretty, but it works.

--- day8/day8.py
+++ day8/day8.py
@@ -1,3 +1,5 @@
+import math
+
 def read_input(path: str) -> tuple[list[list[str]],dict[str:list]]:
     with open(path) as f:
         grid = [[l for l in line.strip("\n")] for line in f.readlines()]
@@ -36,11 +38,49 @@ def part1(path: str):
 
 def vector(p1,p2):
     return [(p2[0]-p1[0]),(p2[1]-p1[1])]
-# def distance(p1,p2):
-#     return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**(1/2)
+def distance(p1,p2):
+    return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**(1/2)
+def normal(p1,p2):
+    v = vector(p1,p2)
+    d = distance(p1,p2)
+    d = float("inf") if d==0 else d
+    return (v[0]/d,v[1]/d)
+def minimal_vector(p1,p2):
+    vec = vector(p1,p2)
+    denom = math.gcd(*vec)
+    return list(map(lambda v: v/denom, vec))
 
-def part2(path: str):
-    pass
+
+def part2(path:str):
+    grid,nodes = read_input(path)
+    locations = set()
+
+    for n in nodes.keys():
+        nodeset = nodes[n]
+
+        for i in range(len(nodeset)):
+            pos1 = nodeset[i]
+            for j in range(i+1,len(nodeset)):
+                pos2 = nodeset[j]
+                vec = minimal_vector(pos1,pos2)
+
+                loc = pos1 # Current location
+                s = 0 # Scale
+                while 0<=loc[0]<len(grid) and 0<=loc[1]<len(grid[0]):
+                    locations.add(loc)
+                    loc = (pos1[0]+s*vec[0], pos1[1]+s*vec[1])
+                    s += 1
+                loc = pos1
+                s = 0
+                while 0<=loc[0]<len(grid) and 0<=loc[1]<len(grid[0]):
+                    locations.add(loc)
+                    loc = (pos1[0]+s*vec[0], pos1[1]+s*vec[1])
+                    s -= 1
+    return len(locations)
+                        
+
+
+
 
 if __name__ == "__main__":
     import sys
