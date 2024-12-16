8de7d6bf43a935939001dfda8d03c8741f7d6511
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2024-12-16 00:08:55 -0600

    Boilerplate

---
+++ boilerplate.py
@@ -0,0 +1,20 @@
+def read_input(path: str):
+    pass
+
+def part1(path: str):
+    pass
+
+def part2(path: str):
+    pass
+
+if __name__ == "__main__":
+    import sys
+    path = sys.argv[1] if len(sys.argv) > 1 else "sample.txt"
+    print("---- Part 1 ----")
+    p1 = part1(path)
+    print("Total:", p1)
+    print("----------------")
+    print("---- Part 2 ----")
+    p2 = part2(path)
+    print("Total:", p2)
+    print("----------------")
